5a785f10b812bdbc48e060780df2d7e44ec6cdca
Pooja <shettypooja2000@gmail.com>
2020-11-04 14:06:24 +0530

    display component created

--- UI Side/src/app/app.module.ts
+++ UI Side/src/app/app.module.ts
@@ -12,30 +12,35 @@ import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
-import {MatExpansionModule} from '@angular/material/expansion'; 
-import {MatSelectModule} from '@angular/material/select';
-import {MatInputModule} from '@angular/material/input';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatDialogModule} from '@angular/material/dialog'
-import {MatTabsModule} from '@angular/material/tabs';
+import { MatExpansionModule } from '@angular/material/expansion'; 
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatDialogModule } from '@angular/material/dialog'
+import { MatTabsModule } from '@angular/material/tabs';
 import { MatTableModule} from '@angular/material/table';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatIconModule} from '@angular/material/icon';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatIconModule } from '@angular/material/icon';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { DisplayComponent } from './display/display.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    DisplayComponent
+    DisplayComponent,
     HeaderComponent,
+    HomeComponent,
+    
+  ],
+  imports: [
     BrowserModule,
     AppRoutingModule,
-
+    BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
-
     MatButtonModule,
     MatTableModule,
     MatTabsModule,
